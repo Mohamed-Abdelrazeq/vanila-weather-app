f1e4ba1bfc60a103a54e7de8755c68f6dd7d9005
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-11-21 01:51:30 +0200

    oop-local storage-apis

---
+++ scripts/app.js
@@ -0,0 +1,58 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+const forecast = new Forecast();
+
+const updateUI = (data) => {
+  // destructure inputs
+  const { cityDetails, weather } = data;
+
+  // update UI
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // icons/images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  const timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeSrc);
+
+  // hide/show card
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+cityForm.addEventListener("submit", (e) => {
+  // prevent default action
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with new city
+  forecast
+    .updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+
+  // set local storage
+  localStorage.setItem("city", city);
+});
+
+if (localStorage.getItem("city")) {
+  forecast
+    .updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
